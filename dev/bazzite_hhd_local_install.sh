dcb4a1568e466ac6826a5753d5d83b962f63d5c3
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-07-27 09:32:36 -0400

    minor update

--- dev/bazzite_hhd_local_install.sh
+++ dev/bazzite_hhd_local_install.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
-# horipad steam controller: 
+# horipad steam controller:
 # edit ~/.steam/steam/config/config.vdf
 # add to SDL_GamepadBind
 # 060000000d0f00009601000000000000,Steam Controller (HHD),a:b0,b:b1,x:b2,y:b3,back:b6,guide:b8,start:b7,leftstick:b9,rightstick:b10,leftshoulder:b4,rightshoulder:b5,dpup:h0.1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,leftx:a0,lefty:a1,rightx:a3,righty:a4,lefttrigger:a2,righttrigger:a5,paddle1:b13,paddle2:b12,paddle3:b15,paddle4:b14,misc2:b11,misc3:b16,misc4:b17,crc:ea35,
@@ -44,7 +44,7 @@ Restart=always
 RestartSec=5
 WorkingDirectory=/var/home/$USER/.local/bin/hhd/venv/bin
 ExecStart=/var/home/$USER/.local/bin/hhd/venv/bin/hhd --user $USER
-Environment="HHD_HORI_STEAM=1"
+Environment="HHD_HORI_STEAM=0"
 Environment="HHD_PPD_MASK=0"
 Environment="HHD_GS_STANDBY=0"
 Environment="HHD_GS_STEAMUI_HALFHZ=0"
